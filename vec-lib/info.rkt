d71adab7ca37ad3556e103fd7e020eae7d48c378
Philip McGrath <philip@philipmcgrath.com>
2022-07-06 14:35:46 -0400

    Split package into "vec" and "vec-lib".

---
+++ vec-lib/info.rkt
@@ -0,0 +1,14 @@
+#lang info
+
+(define pkg-name "vec-lib")
+(define collection "vec")
+(define version "0.0")
+(define pkg-desc
+  "implementation (no documentation) part of \"scribble\"")
+(define pkg-authors
+  '(aeva))
+(define license
+  'Apache-2.0)
+
+(define deps
+  '("base"))
